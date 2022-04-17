6ce185edd1c2882ae20b9fae3beb99d30b8cd69b
Kinnon McAlister <kinnonmcalister@gmail.com>
2022-04-17 15:42:44 -0700

    Split upload page into separate components

--- web/upload_page.ts
+++ web/upload_page.ts
@@ -1,4 +1,4 @@
-import { el } from "redom";
+import { el, RedomComponent } from "redom";
 import { Money, UploadSession } from "../money-web/pkg/money_web";
 import { Table, ColumnView } from "./components";
 import { Page } from "./page";
@@ -6,18 +6,14 @@ import { Page } from "./page";
 
 export class UploadPage implements Page {
     client: Money;
-    session: UploadSession;
-    current_row_count: number;
-    preview: Table;
 
     title: HTMLParagraphElement;
     subtitle: HTMLParagraphElement;
+    upload_select: UploadSelect;
 
     input_field: HTMLDivElement;
     load_field: HTMLDivElement;
 
-    file_field: HTMLInputElement;
-    load_button: HTMLButtonElement;
     error_label: HTMLDivElement;
     show_more_wrapper: HTMLFieldSetElement;
     submit_wrapper: HTMLFieldSetElement;
@@ -28,20 +24,13 @@ export class UploadPage implements Page {
 
     constructor(client: Money) {
         this.client = client;
-        this.session = null;
-        this.preview = null;
-        this.current_row_count = 0;
 
         this.title = null;
         this.subtitle = null;
+        this.upload_select = null;
+
         this.input_field = null;
         this.load_field = null;
-
-        this.show_more_button = null;
-        this.show_more_wrapper = null;
-        this.submit_button = null;
-        this.submit_wrapper = null;
-        this.error_label = null;
 
         this.el = new ColumnView("is-half");
     }
@@ -49,38 +38,12 @@ export class UploadPage implements Page {
     onmount() {
         this.title = el("p", { class: "title is-1" }, "Add Transactions");
         this.subtitle = el("p", { class: "subtitle is-3" }, "Select a file");
-        this.input_field = el("div", { class: "field" }, [
-            el("label.label", "File upload"),
-            el("div.control", this.file_field = el("input", { type: "file", class: "input" }))
-        ]);
-        this.load_field = el(
-            "div",
-            { class: "field" },
-            el("div.control", this.load_button = el("button", { class: "button is-link" }, "Load file"))
-        );
-
-        this.load_button.onclick = evt => {
-            evt.preventDefault();
-
-            var reader = new FileReader();
-            reader.onloadend = _evt => {
-                if (reader == null) {
-                    console.log("Error: reader is null.");
-                    return;
-                }
-
-                this.session = this.client.load_file(reader);
-                this.draw_preview();
-            };
-
-            reader.readAsText(this.file_field.files[0]);
-        };
+        this.upload_select = new UploadSelect(this);
 
         this.el.set_contents([
             this.title,
             this.subtitle,
-            this.input_field,
-            this.load_field
+            this.upload_select
         ]);
     }
 
@@ -88,18 +51,26 @@ export class UploadPage implements Page {
         this.el.set_contents([
             this.title,
             this.subtitle,
-            this.input_field,
-            this.load_field
+            this.upload_select
         ]);
     }
 
-    draw_preview() {
-        this.preview = new Table();
-        this.preview.set_headers(this.session.get_headers().map(h => '"' + h + '"'));
-        this.preview.set_suggestions(
-            this.session.get_header_suggestions(),
-            (column_index, selection) => this.process_update(column_index, selection)
-        );
+    load_file(file: File) {
+        var reader = new FileReader();
+        reader.onloadend = _evt => {
+            if (reader == null) {
+                console.log("Error: reader is null.");
+                return;
+            }
+
+            this.draw_preview(reader);
+        };
+
+        reader.readAsText(file);
+    }
+
+    draw_preview(reader: FileReader) {
+        let session = this.client.load_file(reader);
 
         this.subtitle.innerText = "Select the types of each column";
 
@@ -107,10 +78,82 @@ export class UploadPage implements Page {
         this.el.set_contents([
             this.title,
             this.subtitle,
+            new UploadPreview(this, session)
+        ])
+    }
+
+    draw_submitted() {
+        this.error_label.textContent = "Data submitted.";
+        this.error_label.className = "message-body";
+
+        this.el.set_contents([
+            this.title,
+            new UploadSubmitted()
+        ]);
+    }
+}
+
+
+class UploadSelect implements RedomComponent {
+    el: HTMLDivElement;
+
+    upload_page: UploadPage;
+    file_field: HTMLInputElement;
+    load_button: HTMLButtonElement;
+
+    constructor(upload_page: UploadPage) {
+        this.upload_page = upload_page;
+
+        this.el = el("div", [
+            el("div.field", [
+                el("label.label", "File upload"),
+                el("div.control", this.file_field = el("input", { type: "file", class: "input" }))
+            ]),
+            el(
+                "div.field",
+                el("div.control", this.load_button = el("button", { class: "button is-link" }, "Load file"))
+            )
+        ]);
+
+        this.load_button.onclick = evt => {
+            evt.preventDefault();
+
+            this.upload_page.load_file(this.file_field.files[0]);
+        };
+    }
+}
+
+
+class UploadPreview implements RedomComponent {
+    session: UploadSession;
+    upload_page: UploadPage;
+    current_row_count: number;
+
+    el: HTMLDivElement;
+    table: Table;
+    show_more_button: HTMLButtonElement;
+    show_more_wrapper: HTMLFieldSetElement;
+    submit_button: HTMLButtonElement;
+    submit_wrapper: HTMLFieldSetElement;
+    error_label: HTMLDivElement;
+
+    constructor(upload_page: UploadPage, session: UploadSession) {
+        this.upload_page = upload_page;
+        this.session = session;
+        this.current_row_count = 0;
+
+        this.table = new Table();
+        this.table.set_headers(this.session.get_headers().map(h => '"' + h + '"'));
+        this.table.set_suggestions(
+            this.session.get_header_suggestions(),
+            (column_index, selection) => this.process_update(column_index, selection)
+        );
+
+        this.el = el("div", [
             el("article", { className: "message is-danger" },
                 this.error_label = el("div", { className: "message-body is-hidden" }, "")
             ),
-            this.preview,
+            this.table,
             el("div", { className: "field is-grouped" }, [
                 this.show_more_wrapper = el("fieldset",
                     el("div.control",
@@ -132,58 +175,62 @@ export class UploadPage implements Page {
 
         this.submit_button.onclick = evt => {
             evt.preventDefault();
-            if (this.session.get_selection_error() === undefined) {
+            if (!this.check_error()) {
                 this.session.submit_data();
-                this.draw_submitted();
+                this.upload_page.draw_submitted();
             }
         };
-
-        this.file_field = null;
-        this.load_button = null;
 
         this.add_rows();
     }
 
-    add_rows() {
+    add_rows(): void {
         let total_row_count = this.session.get_row_count();
         let remaining_rows = Math.max(0, total_row_count - this.current_row_count);
         let row_count = Math.min(10, remaining_rows);
-        console.log("Getting rows", this.current_row_count, this.current_row_count + row_count);
-        this.preview.add_rows(
-            this.session.get_row_slice(this.current_row_count, this.current_row_count + row_count)
-        );
-        this.current_row_count += row_count;
+
+        if (row_count > 0) {
+            console.log("Getting rows", this.current_row_count, this.current_row_count + row_count);
+            this.table.add_rows(
+                this.session.get_row_slice(this.current_row_count, this.current_row_count + row_count)
+            );
+            this.current_row_count += row_count;
+        }
 
         if (this.current_row_count == total_row_count) {
             this.show_more_wrapper.setAttribute("disabled", "true");
         }
     }
 
-    process_update(column_index: number, selection: string) {
+    process_update(column_index: number, selection: string): void {
         this.session.update_header_selection(column_index, selection);
+        this.check_error();
+    }
 
+    check_error(): boolean {
         let selection_error = this.session.get_selection_error();
+        console.log("selection error", selection_error);
         if (selection_error !== undefined) {
             this.error_label.textContent = selection_error;
             this.error_label.className = "message-body";
             this.submit_wrapper.setAttribute("disabled", "true");
+            return true;
         } else {
             this.error_label.textContent = "";
             this.error_label.className = "message-body is-hidden";
             this.submit_wrapper.removeAttribute("disabled");
+            return false;
         }
     }
+}
 
-    draw_submitted() {
-        this.error_label.textContent = "Data submitted.";
-        this.error_label.className = "message-body";
+class UploadSubmitted implements RedomComponent {
+    el: HTMLElement;
 
-        this.el.set_contents([
-            this.title,
-            el("article", { className: "message is-primary is-large" }, [
-                el("div.message-header", "Upload complete"),
-                el("div.message-body", "You can now return to the home page")
-            ])
+    constructor() {
+        this.el = el("article", { className: "message is-primary is-large" }, [
+            el("div.message-header", "Upload complete"),
+            el("div.message-body", "You can now return to the home page")
         ]);
     }
 }
