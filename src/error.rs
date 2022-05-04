1628d02bf5c501d4479ae36a01a87e4ab168086a
Kinnon McAlister <kinnonmcalister@gmail.com>
2022-05-03 21:46:13 -0700

    Added macro to decrease MoneyError impl

--- src/error.rs
+++ src/error.rs
@@ -2,57 +2,74 @@ use std::fmt;
 
 use csv_async;
 use diesel;
-use rocket::http::Status;
 use rocket::request::Request;
-use rocket::response::{self, Responder};
 use yansi::Paint;
 
-#[derive(Debug)]
-pub enum MoneyError {
-    IoError(std::io::Error),
-    DbError(diesel::result::Error),
-    CsvError(csv_async::Error),
+macro_rules! error_impl {
+    ($enum_name:ident, $(($err_name:ident, $err_msg:expr, $from_type:path)),+) => (
+        #[derive(Debug)]
+        pub enum $enum_name {
+            $($err_name($from_type)),+
+        }
+
+        impl ::std::fmt::Display for $enum_name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                match &self {
+                    $($enum_name::$err_name(e) => write!(f, concat!($err_msg, ": {}"), e)),+
+                }
+            }
+        }
+
+        impl<'r> ::rocket::response::Responder<'r, 'static> for $enum_name {
+            fn respond_to(self, _: &'r Request<'_>) -> ::rocket::response::Result<'static> {
+                match &self {
+                    $($enum_name::$err_name(e) => warn_!(concat!($err_msg, ": {}"), Paint::default(e))),+
+                }
+                ::rocket::response::Result::Err(::rocket::http::Status::InternalServerError)
+            }
+        }
+
+        $(
+            impl From<$from_type> for $enum_name {
+                fn from(error: $from_type) -> $enum_name {
+                    $enum_name::$err_name(error)
+                }
+            }
+        )+
+    );
 }
 
-impl fmt::Display for MoneyError {
+error_impl!(
+    MoneyError,
+    (IoError, "I/O Error", std::io::Error),
+    (DbError, "Database Error", diesel::result::Error),
+    (CsvError, "CSV Parsing Error", csv_async::Error),
+    (TableError, "Table Access Error", TableError)
+);
+
+#[derive(Debug)]
+pub enum TableError {
+    RowIndex { row: usize, bound: usize },
+    RowLength { length: usize, bound: usize },
+}
+
+impl fmt::Display for TableError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self {
-            MoneyError::IoError(e) => write!(f, "I/O Error: {}", e),
-            MoneyError::DbError(e) => write!(f, "Database Error: {}", e),
-            MoneyError::CsvError(e) => write!(f, "CSV Parsing Error: {}", e),
+            TableError::RowIndex { row, bound } => {
+                write!(f, "Row {} is outside length {}", row, bound)
+            }
+            TableError::RowLength { length, bound } => {
+                write!(
+                    f,
+                    "Row has length {} which differs from column count {}",
+                    length, bound
+                )
+            }
         }
     }
 }
 
-impl<'r> Responder<'r, 'static> for MoneyError {
-    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
-        match &self {
-            MoneyError::IoError(e) => warn_!("I/O Error: {:?}", Paint::default(e)),
-            MoneyError::DbError(e) => warn_!("Database Error: {:?}", Paint::default(e)),
-            MoneyError::CsvError(e) => warn_!("CSV Parsing Error: {:?}", Paint::default(e)),
-        }
-        Err(Status::InternalServerError)
-    }
-}
-
-impl From<std::io::Error> for MoneyError {
-    fn from(error: std::io::Error) -> MoneyError {
-        MoneyError::IoError(error)
-    }
-}
-
-impl From<diesel::result::Error> for MoneyError {
-    fn from(error: diesel::result::Error) -> MoneyError {
-        MoneyError::DbError(error)
-    }
-}
-
-impl From<csv_async::Error> for MoneyError {
-    fn from(error: csv_async::Error) -> MoneyError {
-        MoneyError::CsvError(error)
-    }
-}
-
-impl std::error::Error for MoneyError {}
+impl std::error::Error for TableError {}
 
 pub type Result<T> = std::result::Result<T, MoneyError>;
