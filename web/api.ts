d0baab4472fa81d81f8c737ed411b62949a13226
Kinnon McAlister <kinnonmcalister@gmail.com>
2022-05-09 00:44:30 -0700

    Handle API response format in JS

--- web/api.ts
+++ web/api.ts
@@ -1,3 +1,5 @@
+import { MoneyError } from "./money";
+
 export const HEADER_OPTIONS = [
     "-",
     "Date",
@@ -6,13 +8,53 @@ export const HEADER_OPTIONS = [
     "Amount",
 ];
 
+interface MoneyMsg {
+    status: string,
+    response: any
+}
+
+interface MoneyErrorMsg {
+    status: string,
+    msg: string
+}
+
+type MoneyResponse = MoneyMsg | MoneyErrorMsg;
+
+class MoneyApiError extends MoneyError {
+    endpoint: RequestInfo;
+
+    constructor(message: string, endpoint: RequestInfo) {
+        super(message);
+        this.endpoint = endpoint;
+    }
+}
+
+
 export interface AddUploadResponse {
     upload_id: string,
     headers: string[],
     header_suggestions: string[],
 }
 
+export interface GetUploadRowsResponse {
+    rows: string[][],
+}
+
+async function api_request(uri: RequestInfo, init_data: RequestInit): Promise<any> {
+    let resp = await fetch(uri, init_data)
+        .then(async (resp) => await resp.json() as MoneyResponse);
+
+    if (resp.status == "ok") {
+        return (resp as MoneyMsg).response;
+    } else if (resp.status == "error") {
+        throw new MoneyApiError((resp as MoneyErrorMsg).msg, uri)
+    } else {
+        throw new MoneyApiError(`Unexpected response status: ${resp.status}`, uri)
+    }
+}
+
 export async function add_upload(file_contents: string | ArrayBuffer): Promise<AddUploadResponse> {
-    return await fetch("/api/upload/", { method: "post", body: file_contents })
-        .then(async (resp) => await resp.json() as AddUploadResponse);
+    return await api_request("/api/upload/", { method: "post", body: file_contents }) as AddUploadResponse;
+}
+
 }
