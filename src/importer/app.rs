476d30b4c33f7af4c98fdeb4858c7fde34bec6ed
Kinnon McAlister <kinnonmcalister@gmail.com>
2025-04-09 01:21:59 -0700

    Config parsing

--- src/importer/app.rs
+++ src/importer/app.rs
@@ -1,24 +1,73 @@
-use color_eyre::Result;
+use std::{
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
+
+use color_eyre::{Result, eyre::Context};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
     layout::Rect,
-    style::Stylize,
+    style::{Color, Stylize},
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, Paragraph, Widget, Wrap},
 };
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct AppConfig {
+    import_path: PathBuf,
+}
+
+impl AppConfig {
+    fn load(path: &Path) -> Result<Self> {
+        let mut config_text = String::new();
+
+        File::open(path)
+            .and_then(|mut f| f.read_to_string(&mut config_text))
+            .wrap_err_with(|| format!("Cannot read config file at {:?}", path))?;
+
+        toml::from_str(&config_text).wrap_err("Malformed config file")
+    }
+}
+
+#[derive(Debug)]
+struct AppState {
+    config: AppConfig,
+}
+
+#[derive(Debug)]
+enum StateOption {
+    Ok(AppState),
+    Err(String),
+}
 
 #[derive(Debug)]
 pub struct ImporterApp {
+    data_dir: PathBuf,
+    state: StateOption,
+
     counter: u8,
     exit: bool,
 }
 
 impl ImporterApp {
     pub fn new() -> Self {
+        let data_dir = dirs::data_dir()
+            .expect("OS User data directory missing")
+            .join("money_app");
+
+        let state = match AppConfig::load(&data_dir.join("config.toml")) {
+            Ok(config) => StateOption::Ok(AppState { config }),
+            Err(err) => StateOption::Err(format!("{:#}", err)),
+        };
+
         Self {
+            data_dir,
+            state,
             counter: 0,
             exit: false,
         }
@@ -38,8 +87,6 @@ impl ImporterApp {
 
     fn handle_events(&mut self) -> Result<()> {
         match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
@@ -72,27 +119,44 @@ impl ImporterApp {
 
 impl Widget for &ImporterApp {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
+        let title = Line::from(" Money Importer ".bold());
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
+        let block;
+        let contents;
+        match &self.state {
+            StateOption::Ok(state) => {
+                let instructions = Line::from(vec![
+                    " Decrement ".into(),
+                    "<Left>".blue().bold(),
+                    " Increment ".into(),
+                    "<Right>".blue().bold(),
+                    " Quit ".into(),
+                    "<Q> ".blue().bold(),
+                ]);
 
-        Paragraph::new(counter_text)
-            .centered()
+                block = Block::bordered()
+                    .title(title.centered())
+                    .title_bottom(instructions.centered())
+                    .border_set(border::THICK);
+
+                contents = Text::from(format!("{:#?}", state));
+            }
+            StateOption::Err(err) => {
+                let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
+
+                block = Block::bordered()
+                    .title(title.centered())
+                    .title_bottom(instructions.centered())
+                    .border_set(border::THICK)
+                    .border_style(Color::LightRed);
+
+                contents =
+                    Text::from(format!("Error. Cannot load:\n\n{}", err)).style(Color::LightYellow);
+            }
+        }
+
+        Paragraph::new(contents)
+            .wrap(Wrap { trim: false })
             .block(block)
             .render(area, buf);
     }
