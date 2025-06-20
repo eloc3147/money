3f35eab89cffc679a1f2b33456d713f7e2a3d433
Kinnon McAlister <kinnonmcalister@gmail.com>
2025-06-20 02:26:37 -0700

    Add d3 example graph

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,3 +1,29 @@
 "use strict";
 
-console.log("Yo");
+const rd = await import("https://cdn.jsdelivr.net/npm/redom@4.3.0/+esm");
+const plot = await import("/assets/js/plot.js");
+
+
+class HomePage {
+    constructor() {
+        this.el = rd.el("div", [
+            rd.el("h1.title", "Home Page (TODO)"),
+            new plot.Plot()
+        ]);
+    }
+}
+
+
+class MoneyApp {
+    constructor() {
+        this.el = rd.el("div", [
+            rd.el("nav.navbar", { role: "navigation", "aria-label": "main navigation", "is-primary": "" }, [
+                rd.el("div.navbar-brand", rd.el("span.navbar-item", "Money")),
+                rd.el("div.navbar-menu", rd.el("div.navbar-start", this.navbar_items))
+            ]),
+            this.content = rd.el("div", new HomePage(), { class: "container" })
+        ]);
+    }
+}
+
+rd.mount(document.body, new MoneyApp());
