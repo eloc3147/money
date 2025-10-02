1434bc9c62fda01c7eac22680e2a1c7f109c7b9a
Kinnon McAlister <kinnonmcalister@gmail.com>
2025-10-02 01:56:24 -0700

    Add more filters and improve display

--- web/src/pages/transactions.ts
+++ web/src/pages/transactions.ts
@@ -31,36 +31,6 @@ class DataRow implements RedomComponent {
     }
 }
 
-class Table implements RedomComponent {
-    body: List;
-    el: HTMLElement;
-
-    constructor() {
-        this.body = list("tbody", DataRow);
-
-        this.el = el("table.table.is-bordered.is-striped.is-hoverable.is-fullwidth.sticky-table", [
-            el("thead.stick-thead", el("tr", [
-                el("th", "Account"),
-                el("th", el("abbr", "B. Category", { title: "Base Category" })),
-                el("th", "Category"),
-                el("th", el("abbr", "S. Category", { title: "Source Category" })),
-                el("th", "Income"),
-                el("th", "Type"),
-                el("th", "Date"),
-                el("th", "Amount"),
-                el("th", "ID"),
-                el("th", "Name"),
-                el("th", "Memo"),
-            ])),
-            this.body
-        ]);
-    }
-
-    setTransactions(transactions: string[][]): void {
-        this.body.update(transactions);
-    }
-}
-
 class SelectOption implements RedomComponent {
     el: HTMLElement;
 
@@ -79,7 +49,7 @@ class Select implements RedomComponent {
     el: HTMLElement;
 
     constructor(onChange: (selected: Set<string>) => void) {
-        this.select_el = el("select", { multiple: true });
+        this.select_el = el("select.filter-select", { multiple: true }) as HTMLSelectElement;
         this.select = list(this.select_el, SelectOption);
         this.select.el.onchange = (_e: Event) => {
             // Get selected option labels
@@ -91,7 +61,7 @@ class Select implements RedomComponent {
 
             onChange(selected);
         };
-        this.el = el("div.select.is-multiple", this.select);
+        this.el = el("div.select.is-multiple.is-info.is-small.filter-container", this.select);
     }
 
     update(item: Set<string>): void {
@@ -99,31 +69,95 @@ class Select implements RedomComponent {
     }
 }
 
-type FilterCallback = (accounts: Set<string>) => void;
+type FilterCallback = (
+    accounts: Set<string>,
+    base_categories: Set<string>,
+    categories: Set<string>,
+    source_categories: Set<string>
+) => void;
 
-class TransactionFilters implements RedomComponent {
+class Table implements RedomComponent {
     account_select: Select;
+    base_category_select: Select;
+    category_select: Select;
+    source_category_select: Select;
 
     selected_accounts: Set<string>;
+    selected_base_categories: Set<string>;
+    selected_categories: Set<string>;
+    selected_source_categories: Set<string>;
     select_callback: FilterCallback;
 
+    body: List;
     el: HTMLElement;
 
     constructor(select_callback: FilterCallback) {
         this.account_select = new Select(this.onAccountsUpdate.bind(this));
+        this.base_category_select = new Select(this.onBaseCategoryUpdate.bind(this));
+        this.category_select = new Select(this.onCategoryUpdate.bind(this));
+        this.source_category_select = new Select(this.onSourceCategoryUpdate.bind(this));
 
         this.selected_accounts = new Set();
+        this.selected_base_categories = new Set();
+        this.selected_categories = new Set();
+        this.selected_source_categories = new Set();
         this.select_callback = select_callback;
 
-        this.el = el("div.field", [
-            el("label.label", "Account"),
-            this.account_select
+        this.body = list("tbody", DataRow);
+        this.el = el("table.table.is-bordered.is-striped.is-hoverable.is-fullwidth.sticky-table", [
+            el("thead.stick-thead", el("tr", [
+                el("th", "Account"),
+                el("th", el("abbr", "B. Category", { title: "Base Category" })),
+                el("th", "Category"),
+                el("th", el("abbr", "S. Category", { title: "Source Category" })),
+                el("th", "Income"),
+                el("th", "Type"),
+                el("th", "Date"),
+                el("th", "Amount"),
+                el("th", "ID"),
+                el("th", "Name"),
+                el("th", "Memo"),
+            ])),
+            el("thead", el("tr", [
+                el("th", el("div.field", el("div.control", this.account_select))),
+                el("th", el("div.field", el("div.control", this.base_category_select))),
+                el("th", el("div.field", el("div.control", this.category_select))),
+                el("th", el("div.field", el("div.control", this.source_category_select))),
+                el("th", ""),
+                el("th", ""),
+                el("th", ""),
+                el("th", ""),
+                el("th", ""),
+                el("th", ""),
+                el("th", ""),
+            ])),
+            this.body
         ]);
     }
 
-    set_accounts(accounts: Set<string>): void {
+    setTransactions(transactions: string[][]): void {
+        this.body.update(transactions);
+    }
+
+
+    setAccounts(accounts: Set<string>): void {
         this.account_select.update(accounts);
         this.selected_accounts = accounts;
+    }
+
+    setBaseCategories(base_categories: Set<string>): void {
+        this.base_category_select.update(base_categories);
+        this.selected_base_categories = base_categories;
+    }
+
+    setCategories(categories: Set<string>): void {
+        this.category_select.update(categories);
+        this.selected_categories = categories;
+    }
+
+    setSourceCategories(source_categories: Set<string>): void {
+        this.source_category_select.update(source_categories);
+        this.selected_source_categories = source_categories;
     }
 
     onAccountsUpdate(selected: Set<string>): void {
@@ -131,14 +165,33 @@ class TransactionFilters implements RedomComponent {
         this.pushSelections();
     }
 
+    onBaseCategoryUpdate(selected: Set<string>): void {
+        this.selected_base_categories = selected;
+        this.pushSelections();
+    }
+
+    onCategoryUpdate(selected: Set<string>): void {
+        this.selected_categories = selected;
+        this.pushSelections();
+    }
+
+    onSourceCategoryUpdate(selected: Set<string>): void {
+        this.selected_source_categories = selected;
+        this.pushSelections();
+    }
+
     pushSelections(): void {
-        this.select_callback(this.selected_accounts);
+        this.select_callback(
+            this.selected_accounts,
+            this.selected_base_categories,
+            this.selected_categories,
+            this.selected_source_categories
+        );
     }
 }
 
 export class TransactionsPage implements RedomComponent {
     transactions: TransactionsResponse | null;
-    filters: TransactionFilters;
     table: Table;
     truncated: HTMLElement;
     el: HTMLElement;
@@ -146,15 +199,13 @@ export class TransactionsPage implements RedomComponent {
     constructor() {
         this.transactions = null;
 
-        this.filters = new TransactionFilters(this.updateTable.bind(this));
-        this.table = new Table();
+        this.table = new Table(this.updateTable.bind(this));
         this.truncated = el(
             "article.message.is-warning",
             el("div.message-body", `Truncated to ${DISPLAY_LIMIT} rows`),
             { hidden: true }
         );
         this.el = el("div.container.is-fluid", [
-            this.filters,
             this.table,
             this.truncated
         ]);
@@ -166,28 +217,48 @@ export class TransactionsPage implements RedomComponent {
         }
 
         await this.loadTransactions();
-        this.filters.pushSelections();
+        this.table.pushSelections();
     }
 
     async loadTransactions(): Promise<void> {
         this.transactions = await loadTransactions();
 
         let accounts: Set<string> = new Set();
+        let base_categories: Set<string> = new Set();
+        let categories: Set<string> = new Set();
+        let source_categories: Set<string> = new Set();
         for (const transaction of this.transactions) {
             accounts.add(transaction[0]);
+            base_categories.add(transaction[1]);
+            categories.add(transaction[2]);
+            source_categories.add(transaction[3] || " - ");
         }
 
-        this.filters.set_accounts(accounts);
+        this.table.setAccounts(accounts);
+        this.table.setBaseCategories(base_categories);
+        this.table.setCategories(categories);
+        this.table.setSourceCategories(source_categories);
     }
 
-    updateTable(accounts: Set<string>): void {
+    updateTable(
+        accounts: Set<string>,
+        base_categories: Set<string>,
+        categories: Set<string>,
+        source_categories: Set<string>
+    ): void {
         if (this.transactions === null) {
             return;
         }
 
+        // TODO: When fully implemented, if this takes way longer than 39ms, compare performance against sqlite again
         let rows = this.transactions
             .values()
-            .filter((row, _idx) => accounts.has(row[0]))
+            .filter((row, _idx) => {
+                return accounts.has(row[0])
+                    && base_categories.has(row[1])
+                    && categories.has(row[2])
+                    && source_categories.has(row[3] || " - ");
+            })
             .toArray();
 
         // TODO: Sort
